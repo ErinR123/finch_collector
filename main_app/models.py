2c05975ec6bc8444df82ea1f5d0f548001214f71
Erin Robinson <r.erin2546@gmail.com>
2024-03-09 15:10:13 +1030

    third commit

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Finch(models.Model):
@@ -6,4 +7,8 @@ class Finch(models.Model):
     age = models.IntegerField()
     description = models.TextField(max_length=250)
    
+    def __str__(self):
+        return f"{self.name} ({self.id})"
     
+    def get_absolute_url(self):
+        return reverse("detail", kwargs={'finch_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
 from .models import Finch
 # Create your views here.
 
@@ -20,3 +22,15 @@ def finches_index(request):
 def finches_details(request, finch_id):
    finch = Finch.objects.get(id=finch_id)
    return render(request, 'finches/details.html', {'finch': finch})
+
+class FinchCreate(CreateView):
+   model = Finch
+   fields = '__all__'
+
+class FinchUpdate(UpdateView):
+   model = Finch
+   fields = '__all__'
+
+class FinchDelete(DeleteView):
+   model = Finch
+   success_url = '/finches'
